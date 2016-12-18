997c382ab12bbe0cbcb8876242c1f42f1f8762dc
Ben <berob5@student.monash.edu>
2016-12-18 20:35:02 +1100

    Lel no idea what is going on

--- docs/code/feed.php
+++ docs/code/feed.php
@@ -1,7 +1,8 @@
 <?php
-    // Just testing how to select only latest 10 elements for feed to display
-    $test_posts = ["post1 - info info info", "post2 - info info info", "post3 - info info info", "post4 - info info info", "post5 - info info info", "post6 - info info info", "post7 - info info info", "post8 - info info info", "post9 - info info info", "post10 - info info info"];
-/*
+/*  
+// Just testing how to select only latest 10 elements for feed to display
+$test_posts = ["post1 - info info info", "post2 - info info info", "post3 - info info info", "post4 - info info info", "post5 - info info info", "post6 - info info info", "post7 - info info info", "post8 - info info info", "post9 - info info info", "post10 - info info info"];
+
     for($i = 0; $i < sizeof($test_posts); $i++)
     {
         if($i >= sizeof($test_posts) - 5)
@@ -9,7 +10,7 @@
             echo $test_posts[$i]."\n";    
         };
     };
-*/
+
 $first_run = true;              // First run flag
 $copy_array = null;             // Copy array exists as a reference to check whether the original has updated
 if(sizeof($test_posts>10)){     // If the array is longer than 10, chop it. Later we can change this so that we check whether
@@ -36,7 +37,18 @@ else{
         // todo - as functionality allows, if the feed has changed, echo the new stuff on. logic to be decided
     }
 }
-?>
+
+*/
+
+$mysqli = new mysqli('localhost', 'root', 'cat-pwd', 'CAt_content');
+
+$query = "SELECT * FROM posts";
+
+if(!$result = $mysqli->query($query)){
+    die('There was an error running the query [' . $mysqli->error . ']');
+}
+
+echo $result;
 
 /* NOTES:
 
@@ -49,3 +61,5 @@ For feed to work eventually:
 - need a way to actually authenticate to an admin/reserved page to allow write access to database
 
 */
+?>
+
