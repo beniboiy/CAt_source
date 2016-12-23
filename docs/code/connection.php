f3f5409ac592d26cb81be4f58bf70f47a5efc76f
Ben <berob5@student.monash.edu>
2016-12-23 15:12:49 +1100

    seeing what the script will output

--- docs/code/connection.php
+++ docs/code/connection.php
@@ -19,10 +19,6 @@ $sql = 'SELECT * FROM posts';
 $response = $connection->query($sql);
 $response_text = $response->fetch_assoc();
 
-if ($response_text !== null)
-{
-    echo $response_text['content'] . "<br/>" . $response_text['author'] . "<br/>";
-} else {
-    echo "Error";
-}
+echo '<p>'.$response_text['content'].'<br/>'.$response_text['author'].'</p>';
+
 ?>
